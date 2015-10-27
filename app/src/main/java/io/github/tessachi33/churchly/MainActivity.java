aa5a156bf1d309f789f9977a229fcc1ad1ab3da4
Tessa <Tessachi33@yahoo.com>
2015-10-26 22:32:54 -0700

    Add loginButon clickListener

--- app/src/main/java/io/github/tessachi33/churchly/MainActivity.java
+++ app/src/main/java/io/github/tessachi33/churchly/MainActivity.java
@@ -8,24 +8,28 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mLoginButton;
+    private Button mSignupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mLoginButton =(Button) findViewById(R.id.loginButton);
+        mSignupButton = (Button) findViewById(R.id.signupButton);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+
             }
         });
+    }
     }
 
     }
