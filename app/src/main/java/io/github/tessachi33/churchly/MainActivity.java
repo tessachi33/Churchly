414d869047d72b52c41c055428512b125048c420
Tessa <Tessachi33@yahoo.com>
2015-10-26 22:47:01 -0700

    Add activities and inport Intent

--- app/src/main/java/io/github/tessachi33/churchly/MainActivity.java
+++ app/src/main/java/io/github/tessachi33/churchly/MainActivity.java
@@ -1,13 +1,11 @@
 package io.github.tessachi33.churchly;
 
+
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mLoginButton =(Button) findViewById(R.id.loginButton);
+        mLoginButton = (Button) findViewById(R.id.loginButton);
         mSignupButton = (Button) findViewById(R.id.signupButton);
 
         mLoginButton.setOnClickListener(new View.OnClickListener() {
@@ -39,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
 }
 
 
