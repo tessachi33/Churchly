7224eeb984c4aeb1b21f6aee26584bcd0a791269
Tessa <Tessachi33@yahoo.com>
2015-10-30 10:21:21 -0700

    connect church and user xmls to models

--- app/src/main/java/io/github/tessachi33/churchly/models/User.java
+++ app/src/main/java/io/github/tessachi33/churchly/models/User.java
@@ -4,4 +4,6 @@ package io.github.tessachi33.churchly.models;
  * Created by Tessa on 10/30/15.
  */
 public class User {
+
+
 }
---
+++ app/src/main/java/io/github/tessachi33/churchly/ui/churchActivity.java
@@ -0,0 +1,20 @@
+package io.github.tessachi33.churchly.ui;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import io.github.tessachi33.churchly.R;
+
+public class churchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_church);
+    }
+
+}
