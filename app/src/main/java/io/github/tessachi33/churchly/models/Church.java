84a20d32018d70a3c349d9a692f2bf55907ff46d
Tessa <Tessachi33@yahoo.com>
2015-10-30 14:46:42 -0700

    make church and user class w sqliteopenhelper

--- app/src/main/java/io/github/tessachi33/churchly/models/Church.java
+++ app/src/main/java/io/github/tessachi33/churchly/models/Church.java
@@ -4,11 +4,13 @@ package io.github.tessachi33.churchly.models;
  * Created by Tessa on 10/28/15.
  */
 import android.app.ListActivity;
-
+import android.database.sqlite.SQLiteOpenHelper;
 import com.activeandroid.Model;
 import com.activeandroid.annotation.Column;
 import com.activeandroid.annotation.Table;
 import com.activeandroid.query.Select;
 
-public class Church extends ListActivity {
+public class Church extends SQLiteOpenHelper {
+
+
 }
--- app/src/main/java/io/github/tessachi33/churchly/models/User.java
+++ app/src/main/java/io/github/tessachi33/churchly/models/User.java
@@ -3,7 +3,9 @@ package io.github.tessachi33.churchly.models;
 /**
  * Created by Tessa on 10/30/15.
  */
-public class User {
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class User extends SQLiteOpenHelper {
 
 
 }
