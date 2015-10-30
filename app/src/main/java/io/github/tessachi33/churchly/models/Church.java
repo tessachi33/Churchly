b1a18b503a3eae9ab5e5cc5aab29205c83d3a190
Tessa <Tessachi33@yahoo.com>
2015-10-30 09:13:49 -0700

    Add Active Android

--- app/src/main/java/io/github/tessachi33/churchly/models/Church.java
+++ app/src/main/java/io/github/tessachi33/churchly/models/Church.java
@@ -3,5 +3,10 @@ package io.github.tessachi33.churchly.models;
 /**
  * Created by Tessa on 10/28/15.
  */
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+import com.activeandroid.query.Select;
+
 public class Church {
 }
