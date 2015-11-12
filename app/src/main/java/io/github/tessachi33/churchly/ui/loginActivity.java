a401da23447253ff1262b61d040d92e289ec9fb6
Tessa <Tessachi33@yahoo.com>
2015-11-12 14:06:21 -0800

    Add @Bind in loginActivity

--- app/src/main/java/io/github/tessachi33/churchly/ui/loginActivity.java
+++ app/src/main/java/io/github/tessachi33/churchly/ui/loginActivity.java
@@ -6,11 +6,19 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import io.github.tessachi33.churchly.R;
 
 public class loginActivity extends AppCompatActivity {
+    @Bind(R.id.loginButton) Button mLoginButton;
+    @Bind(R.id.Username) EditText mUsername;
+    @Bind(R.id.Email) EditText mEmail;
+    @Bind(R.id.Password) EditText mPassword;
+    @Bind(R.id.Zipcode) EditText mZipcode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
