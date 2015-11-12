367d5c15b293b8731c4c142056725eec3a801e6b
Tessa <Tessachi33@yahoo.com>
2015-11-12 10:35:24 -0800

    Add Butterknife to signupActivity

--- app/src/main/java/io/github/tessachi33/churchly/ui/signupActivity.java
+++ app/src/main/java/io/github/tessachi33/churchly/ui/signupActivity.java
@@ -6,38 +6,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 import com.parse.Parse;
+import com.parse.ParseUser;
 
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import io.github.tessachi33.churchly.R;
 
 public class signupActivity extends AppCompatActivity {
 
-    private Button mSignupButton;
-    private EditText mSignupName;
-    private EditText mSignupEmail;
-    private EditText mSignupPassword;
-    private EditText mSignupLocation;
+    @Bind(R.id.signupButton) Button mSignupButton;
+    @Bind(R.id.UserName) EditText mUsername;
+    @Bind(R.id.Email) EditText mEmail;
+    @Bind(R.id.Password) EditText mPassword;
+    @Bind(R.id.Zipcode) EditText mZipcode;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         Parse.initialize(this, "hz4ABkxiLqsPDHxUYrHqh0DJAkKVuWTXA2xI4Q2V", "RpUcKjAzyx76UBkwbrWyT2SxRiV7oKGfY39We9GO");
+        ButterKnife.bind(this);
 
-
-        mSignupButton = (Button) findViewById(R.id.signupButton);
 
         mSignupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(signupActivity.this, userActivity.class);
                 startActivity(intent);
+//
+//
+//                Parse user = new ParseUser();
+//                user.setUsername(Username);
+//                user.setEmail(Email);
+//                user.setPassword(Password);
+//                user.Zipcode(Zipcode);
 
-                mSignupName = (EditText) findViewById(R.id.signupName);
-                mSignupEmail = (EditText) findViewById(R.id.signupEmail);
-                mSignupPassword = (EditText) findViewById(R.id.signupPassword);
-                mSignupLocation = (EditText) findViewById(R.id.signupLocation);
 
             }
         });
