7348174d1311a69f0fcfbf18029cda5086afb21e
Tessa <Tessachi33@yahoo.com>
2015-11-16 09:43:23 -0800

    Add User database Model

--- app/src/main/java/io/github/tessachi33/churchly/models/User.java
+++ app/src/main/java/io/github/tessachi33/churchly/models/User.java
@@ -1,7 +1,34 @@
 package io.github.tessachi33.churchly.models;
 
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
 /**
  * Created by Tessa on 11/11/15.
  */
-public class User {
+
+@ParseClassName("_User")
+
+public class User extends ParseObject {
+
+    public User(){
+        super();
+    }
+
+    public String getUsername() {
+        return getString("username");
+    }
+
+    public String getPassword() {
+        return getString("pasword");
+    }
+
+    public String getZipcode() {
+        return getString("Zipcode");
+    }
+
+    public String getEmail() {
+        return getString("email");
+    }
+
 }
