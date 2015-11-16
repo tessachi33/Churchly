4d6f69e1cb29f08ba411db6fde4f44424349c7a1
Tessa <Tessachi33@yahoo.com>
2015-11-16 09:43:02 -0800

    Add Church database Model

--- app/src/main/java/io/github/tessachi33/churchly/models/Church.java
+++ app/src/main/java/io/github/tessachi33/churchly/models/Church.java
@@ -5,9 +5,51 @@ package io.github.tessachi33.churchly.models;
  */
 import android.app.ListActivity;
 
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+import java.util.List;
+
 import io.github.tessachi33.churchly.ui.MainActivity;
 
-public class Church extends MainActivity {
+@ParseClassName("Church")
 
+
+public class Church extends ParseObject {
+
+    public static List<Church> mChurch;
+
+    public Church(){
+        super();
+    }
+
+    public String getName() {
+        return getString("name");
+    }
+
+    public String getImage() {
+        return getString("image");
+    }
+
+    public String getAddress() {
+        return getString("address");
+    }
+
+    public String getPhone() {
+        return getString("phone");
+    }
+
+    public String getReview() {
+        return getString("review");
+    }
+
+    public String getWebsite() {
+        return getString("website");
+    }
+
+
+    public static List<Church> getChurch() {
+        return mChurch;
+    }
 
 }
